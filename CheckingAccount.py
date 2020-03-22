e3c8ab55d446e3756782eb0f1698cd019391b120
PJohnson9 <56849400+PJohnson9@users.noreply.github.com>
2020-03-22 12:36:36 -0400

    Update CheckingAccount.py
    Added openingDate

--- CheckingAccount.py
+++ CheckingAccount.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 class Address:
     def __init__(self, street, city, state, zip):
         self.street = street
@@ -7,8 +9,9 @@ class Address:
 
 
 class CheckingAccount:
-    def __init__(self, accountNumber, name, address, balance = 0):
+    def __init__(self, accountNumber, name, address, balance = 0, openingDate = datetime.now()):
         self.accountNumber = accountNumber
+        self.openingDate = openingDate
         self.name = name
         self.address = address
         self._balance = balance
